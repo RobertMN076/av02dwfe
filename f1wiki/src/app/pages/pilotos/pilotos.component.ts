1389acca88fe693ad094fef0bd22090190e358de
RobertMN076 <Robert.giulio@escolar.ifrn.edu.br>
2025-02-01 20:50:46 -0300

    api consertada

--- f1wiki/src/app/pages/pilotos/pilotos.component.ts
+++ f1wiki/src/app/pages/pilotos/pilotos.component.ts
@@ -1,37 +1,27 @@
 import { Component, NgModule, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Pilotos } from '../../models/pilotos';
-import { PilotosService } from '../../services/pilotos.service';
+import { PilotoService } from '../../services/pilotos.service';
 import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-pilotos',
   standalone: true,
-  imports: [RouterLink, NgFor],
+  imports: [ NgFor],
   templateUrl: './pilotos.component.html',
   styleUrl: './pilotos.component.css'
 })
 export class PilotosComponent implements OnInit{
-  pilotos: Pilotos[] = []
+  pilotos: any[] = []; // Inicializamos como um array vazio
 
-
-  constructor(private pilotosService: PilotosService) {}
+  constructor(private pilotoService: PilotoService) { }
 
   ngOnInit(): void {
-      this.onGetPilotos();
-  }
-
-  onGetPilotos(): void {
-    this.pilotosService.getPilotos().subscribe({
-      next: (dados) => {
-        this.pilotos = dados
-      },
-      error: (erro) => {
-        console.log(erro)
-      },
-      complete: () => {
-        console.log('Chamada Finalizada')
-      }
-    })
+    this.pilotoService.getPilotos().subscribe(data => {
+      // A API Ergast retorna os dados dentro de um objeto MRData
+      this.pilotos = data.MRData.DriverTable.Drivers;
+      console.log(this.pilotos)
+    });
   }
 }
+
--- f1wiki/src/app/services/pilotos.service.ts
+++ f1wiki/src/app/services/pilotos.service.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@angular/core';
-import { Observable, map } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { Pilotos } from '../models/pilotos';
+import { Observable } from 'rxjs';
 
 
 @Injectable({
   providedIn: 'root'
 })
-export class PilotosService {
+export class PilotoService {
+
+  private apiUrl = 'http://ergast.com/api/f1/2024/drivers.json'; // URL da API Ergast
 
   constructor(private http: HttpClient) { }
 
-
-  getPilotos(): Observable<Pilotos[]> {
-    return this.http.get<Pilotos[]>('http://ergast.com/api/f1/2024/drivers.json').pipe(map((retorno: any) => retorno.data));
+  getPilotos(): Observable<any> { // Ajustamos o tipo para any
+    return this.http.get(this.apiUrl);
   }
 }
