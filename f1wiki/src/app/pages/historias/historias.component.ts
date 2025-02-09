77d9bbf7d5862f572fe6ad800fdce242db4f23a3
Lucas Eduardo <lucaseduardo168@gmail.com>
2025-02-09 17:22:39 -0300

    tabela historias

--- f1wiki/src/app/pages/historias/historias.component.ts
+++ f1wiki/src/app/pages/historias/historias.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 
+import { TableModule } from 'primeng/table';
+
+import { HistoriasService } from '../../services/historias.service';
+
 @Component({
   selector: 'app-historias',
   standalone: true,
-  imports: [],
+  imports: [TableModule],
   templateUrl: './historias.component.html',
   styleUrl: './historias.component.css'
 })
 export class HistoriasComponent {
+  historias: any[] = [];
+
+  constructor(private historiaService: HistoriasService) {}
+
+  ngOnInit(): void {
+    this.historiaService.getHistorias().subscribe(data => {
+      this.historias = data;
+    });
+  }
 
 }
